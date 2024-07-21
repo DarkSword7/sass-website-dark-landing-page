afe81d0aab976e771442a47fe1268a5aa6a459a7
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-07-21 16:38:32 +0530

    make banner responsive

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,9 +5,14 @@ export const Banner = () => {
   );]"
     >
       <div className="container">
-        <a href="" className="underline underline-offset-4 font-medium">
-          Explore the demo
-        </a>
+        <p className="font-medium">
+          <span className="hidden sm:inline">
+            Introducing a completely redesigned interface -{" "}
+          </span>
+          <a href="" className="underline underline-offset-4">
+            Explore the demo
+          </a>
+        </p>
       </div>
     </div>
   );
