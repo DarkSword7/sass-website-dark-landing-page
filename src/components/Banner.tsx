29bc0b2fa3f986dedaa57a492c6cae4365eaef27
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-07-21 16:28:00 +0530

    add banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,14 @@
 export const Banner = () => {
-  return null;
+  return (
+    <div
+      className="py-3 text-center bg-[linear-gradient(to_right,rgb(252,_214,_255,_.7),rgb(41,_216,_255,_.7),rgb(255,_253,_128,_.7),rgb(248,_154,_191,_.7),rgb(252,_214,_255,_.7))]
+  );]"
+    >
+      <div className="container">
+        <a href="" className="underline underline-offset-4 font-medium">
+          Explore the demo
+        </a>
+      </div>
+    </div>
+  );
 };
