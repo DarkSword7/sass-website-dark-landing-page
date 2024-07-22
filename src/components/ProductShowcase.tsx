822ac93bc02c253dbe652d43a8229a474f85eff2
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-07-22 19:30:47 +0530

    add product showcase component

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
 import appScreen from "../assets/images/app-screen.png";
 
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CA8] py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-center font-bold text-5xl sm:text-6xl tracking-tighter">
+          Intuitive interface
+        </h2>
+        <div className="max-w-xl mx-auto">
+          <p className="text-center text-xl text-white/70 mt-5">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress, motivate your efforts, and celebrate your successes,
+            one task at a time.
+          </p>
+        </div>
+        <div className="flex justify-center">
+          <Image
+            src={appScreen}
+            alt="The Product Screenshot"
+            className="mt-14"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
