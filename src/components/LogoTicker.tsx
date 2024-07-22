9c9aef05756b878304386e45566a8324183ae593
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-07-22 18:39:00 +0530

    add LogoTicker section

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -4,6 +4,7 @@ import echoLogo from "../assets/images/echo.png";
 import celestialLogo from "../assets/images/celestial.png";
 import pulseLogo from "../assets/images/pulse.png";
 import apexLogo from "../assets/images/apex.png";
+import Image from "next/image";
 
 const images = [
   { src: acmeLogo, alt: "Acme Logo" },
@@ -15,5 +16,26 @@ const images = [
 ];
 
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-xl text-center text-white/70">
+          Trusted by the world&apos;s most innovative teams
+        </h2>
+        <div className="overflow-hidden flex justify-center mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
+          <div className="flex gap-16">
+            {" "}
+            {images.map((image, index) => (
+              <Image
+                key={index}
+                src={image.src}
+                alt={image.alt}
+                className="flex-none h-8 w-auto"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
