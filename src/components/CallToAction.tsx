b63506fda1c85feed996b0e4b9082281d378a1db
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-07-23 19:05:01 +0530

    chore: Add CallToAction and Footer components with styles

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,3 +1,39 @@
+import helixImage from "../assets/images/helix2.png";
+import emojiStarImage from "../assets/images/emojistar.png";
+import Image from "next/image";
+
 export const CallToAction = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24 text-center overflow-hidden">
+      <div className="container max-w-xl relative">
+        <Image
+          src={helixImage}
+          alt="Helix"
+          className="absolute top-6 left-[calc(100%+36px)]"
+        />
+        <Image
+          src={emojiStarImage}
+          alt="Emoji star"
+          className="absolute -top-[120px] right-[calc(100%+24px)]"
+        />
+        <h2 className="text-center text-5xl sm:text-6xl font-bold tracking-tighter">
+          Get instant access
+        </h2>
+        <p className="text-xl text-white/70 mt-5">
+          Celebrate the joy of accomplishment with an app designed to track your
+          progress and motivate your efforts.
+        </p>
+        <form className="flex flex-col gap-2.5 mt-10 max-w-sm mx-auto sm:flex-row">
+          <input
+            type="email"
+            placeholder="your@email.com"
+            className="h-12 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9CA3AF] sm:flex-1"
+          />
+          <button className="h-12 bg-white text-black text-xl rounded-lg px-5">
+            Get access
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,32 @@
+import InstaIcon from "../assets/icons/insta.svg";
+import XIcon from "../assets/icons/x.svg";
+import TiktokIcon from "../assets/icons/tiktok.svg";
+import YoutubeIcon from "../assets/icons/youtube.svg";
+
 export const Footer = () => {
-  return null;
+  return (
+    <footer className="py-5 bg-black text-white/60 border-t border-white/20">
+      <div className="container">
+        <div className="flex flex-col sm:flex-row sm:justify-between gap-4">
+          <div className="text-center">
+            © 2024 Your Company, Inc. All rights reserved
+          </div>
+          <ul className="flex justify-center gap-2.5">
+            <li>
+              <XIcon />
+            </li>
+            <li>
+              <InstaIcon />
+            </li>
+            <li>
+              <TiktokIcon />
+            </li>
+            <li>
+              <YoutubeIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
