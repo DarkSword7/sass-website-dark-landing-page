86d264301fc714347cbf60fb372a7dd06a89a534
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-07-23 17:26:02 +0530

    Add FAQs component and styles

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -1,3 +1,9 @@
+"use client";
+import React from "react";
+import PlusIcon from "../assets/icons/plus.svg";
+import MinusIcon from "../assets/icons/minus.svg";
+import clsx from "clsx";
+
 const items = [
   {
     question: "What payment methods do you accept?",
@@ -20,7 +26,52 @@ const items = [
       "Security is our top priority. We use state-of-the-art encryption and comply with the best industry practices to ensure that your data is stored securely and accessed only by authorized users.",
   },
 ];
+const AccordionItem = ({
+  question,
+  answer,
+}: {
+  question: string;
+  answer: string;
+}) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <div
+      onClick={() => setIsOpen(!isOpen)}
+      key={question}
+      className="py-7 border-b border-white/30"
+    >
+      <div className="flex items-center">
+        <span className="flex-1 text-lg font-bold cursor-pointer">
+          {question}
+        </span>
+        {isOpen ? <MinusIcon /> : <PlusIcon />}
+      </div>
+      <div
+        className={clsx("mt-4", {
+          hidden: !isOpen,
+          "": isOpen === true,
+        })}
+      >
+        {answer}
+      </div>
+    </div>
+  );
+};
 
 export const FAQs = () => {
-  return null;
+  return (
+    <div className="bg-black text-white bg-gradient-to-b from-[#5D2CA8] to-black py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-center text-5xl sm:text-6xl sm:max-w-[648px] mx-auto font-bold  tracking-tighter">
+          Frequently asked questions
+        </h2>
+        <div className="mt-12 max-w-[648px] mx-auto">
+          {items.map(({ question, answer }) => (
+            <AccordionItem key={question} question={question} answer={answer} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
