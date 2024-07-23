c9d6ea075b7c0157a0240d9389fd4e9883d808f7
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-07-23 22:01:28 +0530

    add parallex effect in call to action component

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,21 +1,37 @@
+"use client";
 import helixImage from "../assets/images/helix2.png";
 import emojiStarImage from "../assets/images/emojistar.png";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const CallToAction = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "end end"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [40, -40]);
   return (
-    <div className="bg-black text-white py-[72px] sm:py-24 text-center overflow-hidden">
+    <div
+      className="bg-black text-white py-[72px] sm:py-24 text-center"
+      ref={containerRef}
+    >
       <div className="container max-w-xl relative">
-        <Image
-          src={helixImage}
-          alt="Helix"
-          className="absolute top-6 left-[calc(100%+36px)]"
-        />
-        <Image
-          src={emojiStarImage}
-          alt="Emoji star"
-          className="absolute -top-[120px] right-[calc(100%+24px)]"
-        />
+        <motion.div style={{ translateY }}>
+          <Image
+            src={helixImage}
+            alt="Helix"
+            className="absolute top-6 left-[calc(100%+36px)]"
+          />
+        </motion.div>
+        <motion.div style={{ translateY }}>
+          <Image
+            src={emojiStarImage}
+            alt="Emoji star"
+            className="absolute -top-[120px] right-[calc(100%+24px)]"
+          />
+        </motion.div>
         <h2 className="text-center text-5xl sm:text-6xl font-bold tracking-tighter">
           Get instant access
         </h2>
