9cc475b7b5786ab40fb6c548a0f263d1539b09d1
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-07-23 21:33:06 +0530

    add scroll animation on product showcase component

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,9 +1,20 @@
+"use client";
 import Image from "next/image";
 import appScreen from "../assets/images/app-screen.png";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
+  const imageRef = useRef<HTMLImageElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: imageRef,
+    offset: ["start end", "end end"],
+  });
+
+  const rotateX = useTransform(scrollYProgress, [0, 1], [20, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
   return (
-    <div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CA8] py-[72px] sm:py-24">
+    <div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CA8] py-[72px] sm:py-24 overflow-hidden">
       <div className="container">
         <h2 className="text-center font-bold text-5xl sm:text-6xl tracking-tighter">
           Intuitive interface
@@ -15,13 +26,21 @@ export const ProductShowcase = () => {
             one task at a time.
           </p>
         </div>
-        <div className="flex justify-center">
+        <motion.div
+          style={{
+            opacity: opacity,
+            rotateX: rotateX,
+            transformPerspective: "800px",
+          }}
+          className="flex justify-center"
+        >
           <Image
             src={appScreen}
             alt="The Product Screenshot"
             className="mt-14"
+            ref={imageRef}
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
